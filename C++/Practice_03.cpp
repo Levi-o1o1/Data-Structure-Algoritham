769983fef888b56ed04c6061f8e2c3fb9772ae5d
Ayush🕸️ <ayux.exe@gmail.com>
2026-05-17 20:13:08 +0530

    for loop 006

--- C++/Practice_03.cpp
+++ C++/Practice_03.cpp
@@ -11,3 +11,17 @@ int main(){
     cout << "odd sum is = " << oddSum << endl;
     return 0;
 }
+
+// sum of even numbers 
+
+int main(){
+    int n = 15;
+    int evenSum = 0;
+    for(int i = 1; i<=n; i++){
+        if(i%2 == 0){
+            evenSum += i;    // output is 56 
+        }
+    }
+    cout << "even sum is ="<< evenSum << endl;
+    return 0;
+}
