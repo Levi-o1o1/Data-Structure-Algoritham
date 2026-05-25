8086689786e1acdde0ce28c7e8db334a2ac4890c
Ayush🕸️ <ayux.exe@gmail.com>
2026-05-25 19:33:21 +0530

    for loop 003

--- C++/08_for_loop.cpp
+++ C++/08_for_loop.cpp
@@ -10,10 +10,22 @@ int main(){
     
     return 0;
 }
-
+// add incress 2 num
 int main(){
     int n = 5;
     for (int i =1; i<=n; i = i+2)
     cout << i << " ";
 
+}
+
+// final value sum program 
+
+int main(){
+    int n = 3;
+    int sum = 0;
+    for (int i =1; i<=n; i++){
+        sum += i;
+    }
+    cout << " sum = "<< sum << endl;
+    return 0;
 }
