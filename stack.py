25ef746d563750f61cef86c547b728831e31d7a1
Levi-o1o1 <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-18 21:16:47 +0530

    pop explain 003

--- stack.py
+++ stack.py
@@ -6,6 +6,15 @@ stack.append(3)
 
 print(stack)
 
-# peek 
+# peek are used to see top element of stack and top element of stack is 3 because of LIFO 
+
 topElement = stack[-1]
 print("stack : ", topElement)
+
+# pop for remove last element from stack and note pop also return object 
+
+popElement = stack.pop()
+print("pop element", popElement)
+
+# stack after popped top element of stack 
+print("stack after pop ", stack)
