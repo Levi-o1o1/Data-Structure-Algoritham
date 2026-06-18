d9c83cb0384e6db2530436890146faf5661b2e9f
Levi-o1o1 <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-18 21:12:06 +0530

    peek explain  002

--- stack.py
+++ stack.py
@@ -1,8 +1,11 @@
 stack = []
-
+# this is push method to push things in stack , push use to insert 
 stack.append(1)
 stack.append(2)
 stack.append(3)
 
 print(stack)
 
+# peek 
+topElement = stack[-1]
+print("stack : ", topElement)
