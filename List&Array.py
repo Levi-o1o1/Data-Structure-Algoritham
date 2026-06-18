de63053624a5b199b36b1d282720ea6fc546ba76
Levi-o1o1 <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-18 22:09:14 +0530

    array add

---
+++ List&Array.py
@@ -0,0 +1,19 @@
+myarray = [90,12,10,89,12,9,12]
+
+minVal = myarray [0]
+
+for  i in myarray:
+    if i < minVal:
+     minVal = i
+
+print("small value is :", minVal)
+
+
+# basic array operations >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+x = [1,5,6,7,3]
+
+
+x.append(2)
+
+x.sort()
+print(x)
