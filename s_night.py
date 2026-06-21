d1f0bd923ce080796e886620a9600135257abe18
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-21 21:51:22 +0530

    adding starry night 02

--- s_night.py
+++ s_night.py
@@ -14,3 +14,10 @@ def draw_star(points, size, col, x, y):
     t.end_fill()
 
 # main code 
+t.Screen() .bgcolor('dark blue')
+
+while True:
+    ranPts = randint(2,5) * 2 + 1
+    ranSize = randint(10, 50)
+    ranCol = (random(), random(), random())
+    ranX
