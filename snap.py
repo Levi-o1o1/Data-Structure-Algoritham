cf6c8d4c0905cba2906259f392a08f9b9ad882b2
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-21 22:00:31 +0530

    snap 002

--- snap.py
+++ snap.py
@@ -4,3 +4,11 @@ from tkinter import Tk, Canvas, HIDDEN, NORMAL
 
 def next_shape():
     global shape
+    global previous_color
+    global current_color
+
+    previous_color = current_color
+
+    
+    C.delete(shape)
+    if len(s)
