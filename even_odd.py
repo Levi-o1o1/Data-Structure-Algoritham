da37b7e66b50aa0e0144d580cd7ab3f853c8f2a8
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-22 14:07:49 +0530

    problem two solve

--- even_odd.py
+++ even_odd.py
@@ -1,7 +1,31 @@
-value =int(input("Enter a Number :"))
+# value =int(input("Enter a Number :"))
 
 
-if value % 2==0:
-    print(value, "Even Number")
+# if value % 2==0:
+#     print(value, "Even Number")
+# else:
+#     print(value, "Odd Number")
+
+# greeting function for every new person :
+
+
+greet = input("Enter your name :")
+
+if greet != input:
+    print("hello", greet)
+
+
+# Take 2 numbers as input and print the largest number.
+
+num1 = int(input("Enter number  1 only :"))
+num2 = int(input("Enter  number 2 only :"))
+
+if num1 > num2:
+    print(num1, " is Bigger")
+elif  num2 > num1:
+    print(num2, "is Begger")
 else:
-    print(value, "Odd Number")
+    print("enter number only ")
+
+
+
