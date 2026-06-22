f75bfbc71d6bdf786c932852462e628bc2065014
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-22 15:32:04 +0530

    type cast 002

--- fun_with_python/type_cast.py
+++ fun_with_python/type_cast.py
@@ -5,3 +5,10 @@ print(a + int(b))
 
 
 # this is type cast to convert string to int and other data type use of this fun int(), float() ..etc...
+
+
+# implicit in this python already convert the datatype by own
+
+c = 1.9
+d = 9
+print(c + 9)
