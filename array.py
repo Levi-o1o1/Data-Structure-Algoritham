3d0d996cfa90c02616f16b6a09330b2cec977aa9
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-22 21:39:46 +0530

    Array 002

--- array.py
+++ array.py
@@ -1,19 +1,7 @@
 import array as a
 
 val = a.array("i", [1,2,3,4,5,6,])
-
-for i in range(0,6):
-     print(val[i], end=" ")
-
-print("\n")
-
-for x in val:
-     print(x, end=' , ')
-
-print("\n")
-print(val.typecode)
-
-val.reverse()
-
+val.insert(1,50 )
+val.append(30)
 for i in range(0,6):
      print(val[i], end=" ")
