9eacf0388550656c5509ca2e261c7c618597ab1f
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-22 20:52:33 +0530

    string method 002

--- fun_with_python/stringMethod.py
+++ fun_with_python/stringMethod.py
@@ -3,3 +3,12 @@ print(len(a))
 print(a.upper())
 print(a.lower())
 print(a.capitalize())
+
+b = "raj"
+if a != b:
+    print(a.replace("ayush", "john"))
+    c = a
+
+    
+print(a.capitalize())
+print(c)
