6919a4c99c08f6a9227737f4490c311fe2ea2717
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-22 14:47:27 +0530

    currecy add

--- even_odd.py
+++ even_odd.py
@@ -9,23 +9,32 @@
 # greeting function for every new person :
 
 
-greet = input("Enter your name :")
+# greet = input("Enter your name :")
 
-if greet != input:
-    print("hello", greet)
+# if greet != input:
+#     print("hello", greet)
 
 
 # Take 2 numbers as input and print the largest number.
 
-num1 = int(input("Enter number  1 only :"))
-num2 = int(input("Enter  number 2 only :"))
+# num1 = int(input("Enter number  1 only :"))
+# num2 = int(input("Enter  number 2 only :"))
 
-if num1 > num2:
-    print(num1, " is Bigger")
-elif  num2 > num1:
-    print(num2, "is Begger")
+# if num1 > num2:
+#     print(num1, " is Bigger")
+# elif  num2 > num1:
+#     print(num2, "is Begger")
+# else:
+#     print("enter number only ")
+
+# Input currency in rupees and output in USD.
+
+curr = int(input("Enter value :"))
+dollar_rate = 96
+if curr :
+   out = curr / dollar_rate
 else:
-    print("enter number only ")
+    print("worng value :")
 
-
+print(out, "doller")
 
