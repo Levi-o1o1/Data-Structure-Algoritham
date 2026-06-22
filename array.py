a1878aa8eb20b92d62528f555ea88fa01c2a1d06
Ayush🕸️ <ayux.exe@gmail.com>
2026-06-22 21:50:09 +0530

    Array 004

--- array.py
+++ array.py
@@ -3,5 +3,8 @@ import array as a
 val = a.array("i", [1,2,3,4,5,6,])
 val.insert(1,50 )
 val.append(30)
+val[3] = 300
+
+copyArray = a.array(val.typecode, (x for x in val))
 for i in range(0,len(val)):
-     print(val[i], end=" ")
+     print(copyArray[i], end=" ")
