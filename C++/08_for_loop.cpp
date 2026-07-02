e4884c2bba1c385358c93cc4abdf029990f98666
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-02 19:20:00 +0530

    for loop 001

---
+++ C++/08_for_loop.cpp
@@ -0,0 +1,12 @@
+//  for loop formula 
+// for(initialisation; condition; updattion){
+//      work 
+// }
+
+int main(){
+    int n = 5;
+    for(int i= 1; i<= n ; i++)
+    cout << i << " ";
+    
+    return 0;
+}
