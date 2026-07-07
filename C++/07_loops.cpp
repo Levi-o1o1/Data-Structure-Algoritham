a33212d8cbb360d4f5d52de68745fc5765ed4680
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-07 18:34:56 +0530

    do while loop breakdown

--- C++/07_loops.cpp
+++ C++/07_loops.cpp
@@ -11,3 +11,16 @@ int main(){
     cout << endl;
     return 0;
 }
+
+//breakdown loop work
+
+int main(){
+    int count = 1; // count value is 1 here is starting value
+
+    while(count <= 3){        // we want end the loop when value is less than 3 or 3 
+        cout << count << "\n";  // this line print the count 
+        count++;         // add count with 1 every time when we run the loop so its add 1 + 1 = 2 than 2 + 1 = 3 
+    }
+    cout << endl; // end the loop 
+    return 0;
+}
