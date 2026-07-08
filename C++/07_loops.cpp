d1f75d069e29cef09a905d7d862d79a1383d10b5
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-08 18:28:28 +0530

    do while loop

---
+++ C++/07_loops.cpp
@@ -0,0 +1,13 @@
+// these are 3 type of loops while, for , or do while loop mostly we use for loop 
+// Loops while loop 
+
+int main(){
+    int a = 1;
+
+    while(a <= 15){
+        cout << a << "\n";
+        a++;
+    }
+    cout << endl;
+    return 0;
+}
