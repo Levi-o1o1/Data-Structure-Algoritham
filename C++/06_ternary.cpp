39aae21dbd4333cf4126f9405fadbe6a8fec4885
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-10 17:52:20 +0530

    if else ternary op

---
+++ C++/06_ternary.cpp
@@ -0,0 +1,11 @@
+// Ternary statement are use to use simple line of if else programs 
+// formula to wirte ternary op [condition ? stt1 : strr2 ;]
+
+int main(){
+    int n = 45;
+
+    cout << (n >= 0 ? "postive" : "negative") << endl:
+    return 0;
+
+
+}
