e03118a6eb2137f508dd77b2ca93f6964f88a847
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-15 11:39:34 +0530

    starting with c++

---
+++ C++/data_types.cpp
@@ -0,0 +1,18 @@
+// int takes 4 bytes memorey 
+// char takes 1 bytes
+// float takes 4 bytes
+// bool takes 1 bytes
+// double takes 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 25;
+    char grade = "a";
+    float PI = 3.14f;
+    bool isSafe = true;
+
+    cout << isSafe << endl;                  // true -> 1 & false -> 0
+
+}
