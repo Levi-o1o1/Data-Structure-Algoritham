ab63777ad284af24678c768ba957ac27c245e300
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-15 15:56:57 +0530

    odd even

--- C++/05_conditional.cpp
+++ C++/05_conditional.cpp
@@ -24,3 +24,19 @@ int main(){
 
     return 0;
 }
+
+// numbers even or odd finder 
+
+
+int main (){
+    int a ;
+    cout << "Enter the value you want to check ::" << endl;
+    cin >> a ;
+
+    if (a % 2 == 0){
+        cout << "The number is even ::" << endl;
+    } else {
+        cout << "the number is odd ::" << endl;
+    }
+    return 0;
+}
