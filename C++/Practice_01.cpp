625c6d70d96ca15a662c086cd9f0ed6e9be116e7
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-15 14:11:04 +0530

    practice commplete

--- C++/Practice_01.cpp
+++ C++/Practice_01.cpp
@@ -2,3 +2,18 @@
 // 1) take input from user
 // 2) sum both value
 // 3) print sum in output window
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a , b ;
+    cout<< "Enter the first number :"<< endl;
+    cin >> a;
+    cout<< "Enter the second number :"<< endl;
+    cin >> b;
+    int sum = a+b ;
+
+    cout<< "sum is :"<< sum << endl;
+    return 0;
+}
