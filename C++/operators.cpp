b4708f5365dff25fc200268548932205b0addbf7
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-15 12:15:11 +0530

    starting with c++ 004

---
+++ C++/operators.cpp
@@ -0,0 +1,11 @@
+// operators 1) arithmetic =  +, -, *, / , moduleo op % ,
+int main (){
+    int a = 10, b = 5;
+    cout << "sum = " << (a+b) << endl;           // output is 15
+    cout << "dirfference = " << (a-b) << endl;   // output is 5
+    cout << "product = " << (a*b) << endl;       // output is 50 
+    cout << "division = "<< (a/b) << endl;       // output is 2
+    cout << "modulo = " << (a%b) << endl;        // output is 0 
+    return 0;
+}
+// 2) logical op 
