ed0f9953eb5a6881da1e14095fa9b36392592392
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-15 12:04:00 +0530

    starting with c++ 003

---
+++ C++/input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//  input in c++ to take data from user we use input funciton its take input from user and show output in terminal
+
+int main(){
+    int age;
+    cout << "Enter the age : ";
+    cin >> age ;
+    cout << "Your age is : " << age << endl;
+    return 0;
+}
+
+//  for double value 
+
+int main(){
+    double price ;
+    cout << "Enter the pirce  : ";
+    cin >> price;
+    cout << "Your price is : " << price << endl;
+    return 0;
+}
