c2bfbbe13b4baaa7c29c196350f2b8c87c23d1ca
Ayush🕸️ <ayux.exe@gmail.com>
2026-07-16 09:42:45 +0530

    add while loop 004

---
+++ C++/09_whileLoop.cpp
@@ -0,0 +1,12 @@
+// program of print 1 to 10 number and last a satement quate
+int main() {
+    int i = 1;
+    int n = 10;
+    while(i <= n){
+        std::cout<< i << "\n";
+        i++;
+    }
+    std::cout<< "happy death day :";
+
+    return 0;
+}
