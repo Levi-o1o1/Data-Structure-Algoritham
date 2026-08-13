7dcbf8d2dd29575908e89b2c310be1394d51c1f0
Ayush🕸️ <ayux.exe@gmail.com>
2026-08-13 04:14:07 +0000

    fucntion 005

--- C++/11_function.cpp
+++ C++/11_function.cpp
@@ -8,7 +8,7 @@ double sum(double a, double b){
     return s;
 }
 // min of 2 num
-int minOfTwo(int a, int b){
+int minOfTwo(int a, int b){    // int a or b called our parameters
     if(a < b){
         return a;
     } else {
@@ -16,6 +16,23 @@ int minOfTwo(int a, int b){
     }
 }
 int main(){
-    cout << "min = " << minOfTwo(5 , 3) << endl;
+    cout << "min = " << minOfTwo(5 , 3) << endl;   // this vlaues called arguments
+    return 0;
+}
+
+
+
+// calculate factorial of n
+
+int factN(int n){
+    int fact = 1;
+    for(int i = 1; i<=n; i++){
+        fact *= i;
+    }
+    return fact; 
+}
+int main(){
+    cout << factN(5) << endl;
+    cout << factN(4) << endl;
     return 0;
 }
