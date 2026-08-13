4147b7e04860609d86ca78e4ae641314adb1a8a6
Ayush🕸️ <ayux.exe@gmail.com>
2026-08-13 03:59:43 +0000

    fucntion 003

--- C++/11_function.cpp
+++ C++/11_function.cpp
@@ -7,7 +7,15 @@ double sum(double a, double b){
     double s = a + b;
     return s;
 }
-
+// min of 2 num
+int minOfTwo(int a, int b){
+    if(a < b){
+        return a;
+    } else {
+        return b;
+    }
+}
 int main(){
+    cout << "main = " << minOfTwo
     return 0;
 }
