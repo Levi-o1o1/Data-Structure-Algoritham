b6fa0c53ae39d1e3ba11d167a75b62f3b2c2e53b
Ayush🕸️ <ayux.exe@gmail.com>
2026-08-13 03:57:37 +0000

    fucntion 002

--- C++/11_function.cpp
+++ C++/11_function.cpp
@@ -1,3 +1,13 @@
 // fucntion are use to do something many times 
 
+# include <iostream>
+using nmaespace std;
 
+double sum(double a, double b){
+    double s = a + b;
+    return s;
+}
+
+int main(){
+    return 0;
+}
